b5f8f299e048e82a58b1bd061f2c4127b4d32094
tutukill <tutukill@gmail.com>
2018-11-28 16:39:02 +0800

    add rotate

--- js/js.js
+++ js/js.js
@@ -1,10 +1,33 @@
-$(document).ready( function() {
-  var $draggable = $('.tooltipcontainer').draggabilly();
-  function listener() {
-	  var draggie = $(this).data('draggabilly');
-	  console.log( 'eventName happened', draggie.position.x, draggie.position.y );
-  }
-  $draggable.on( 'dragStart', function() {
+$(function() {
 
-  });
+	var draggable=$('.tag_shape').draggabilly();
+
+
+	function listener() {
+		var draggie = $(this).data('draggabilly');
+		var tag_line = $(draggie.element).prev('.tag_line')
+
+		var x = draggie.position.x+90 /*加上标签宽度的一半*/
+		var y = draggie.position.y+98 /*加上标签高度的*/
+
+		var a = Math.pow(x,2) + Math.pow(y,2)
+		var z = Math.sqrt(a)
+
+		
+		var zdeg = Math.asin(x/z)*180/Math.PI  /*求角度*/
+
+		if(y>0){
+			zdeg = Math.acos(x/z)*180/Math.PI+90
+		}
+
+		tag_line.css({'transform':'rotate(' + zdeg + 'deg)',
+					     'height':z
+					  })
+
+		console.log( x,y,z,zdeg);
+	}
+
+	draggable.on( 'dragMove', listener );
+
+
 });
